77ed9673b0a6ed83fc2e4fbe1e62fe1e996748d0
Ahmed Hassan <106224867+Capa11@users.noreply.github.com>
2024-12-09 13:00:23 +0200

    updating error404

--- client/src/components/Error404/Error404.jsx
+++ client/src/components/Error404/Error404.jsx
@@ -1,11 +1,13 @@
 import "./style.css";
+import { useNavigate } from 'react-router-dom';
 
 function Error404() {
+  const navigate = useNavigate();
   return (
         <div className="container">
         <span className="errmessage">404 Page not Found</span>
         <span>your visited page not found.You may go home page</span>
-        <button className="btn">Back to Home Page</button>
+        <button className="btn" onClick={() => navigate('/')}>Back to Home Page</button>
       </div>
   );
 }
