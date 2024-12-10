dbf069358f2481c19193d6b7ad74702b6da405cc
noor hesham <norhsham02@gmail.com>
2024-12-10 04:23:24 +0200

    broducts , orders , user model

--- server/models/OrdersModel.js
+++ server/models/OrdersModel.js
@@ -1,1 +1,120 @@
 const mongoose = require("mongoose");
+
+const OrdseSchema = new mongoose.Schema(
+  {
+    customerName: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: function (v) {
+          return validator.isMobilePhone(v, "ar-EG");
+        },
+        message: "{VALUE} is not a valid phone number",
+      },
+    },
+    customerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+    },
+    products: {
+      type: [mongoose.Schema.Types.ObjectId],
+      required: true,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+    orderDate: {
+      type: Date,
+      default: Date.now,
+    },
+    status: {
+      type: String,
+      enum: ["Pending", "Shipped", "Delivered", "canceled"],
+      default: "Pending",
+    },
+    paymentMethod: {
+      type: String,
+      enum: ["cash on delivery", "vodafone cash", "instapay"],
+      required: true,
+    },
+    deliveryAddress: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "addresses",
+    },
+    adressText: {
+      type: String,
+      required: true,
+    },
+    deliveryStatus: {
+      type: String,
+      enum: ["Pending", "In Transit", "Delivered", "Returned", "Cancelled"],
+      default: "Pending",
+    },
+    isReturn: {
+      type: Boolean,
+      default: false,
+    },
+    returnRequestDate: {
+      type: Date,
+      default: null,
+    },
+    returnRequestStatus: {
+      type: String,
+      enum: ["Pending", "Requested", "Canceled", "Returned"],
+      default: "Pending",
+    },
+    returnedProduct: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "products",
+      default: null,
+    },
+
+    returnMaxTime: {
+      type: Date,
+      default: null,
+    },
+
+    EditMaxTime: {
+      type: Date,
+      default: null,
+    },
+
+    deliveryPhoneNumber: {
+      type: String,
+      validate: {
+        validator: function (v) {
+          return validator.isMobilePhone(v, "ar-EG");
+        },
+        message: "{VALUE} is not a valid phone number",
+      },
+    },
+
+    deliveredDate: {
+      type: Date,
+      default: null,
+    },
+    canceledDate: {
+      type: Date,
+      default: null,
+    },
+    canceledReason: {
+      type: String,
+      default: null,
+    },
+    trackingNumber: {
+      type: String,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("orders", OrdseSchema);
--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -1,1 +1,95 @@
 const mongoose = require("mongoose");
+import Wishlist from "./../../client/src/components/wishList/Wishlist";
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      minlength: 6,
+      validate: {
+        validator: function (v) {
+          return /^[a-zA-Z0-9]+$/.test(v);
+        },
+        message: "Username can only contain alphanumeric characters",
+      },
+    },
+
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validator: {
+        validator: validator.isEmail,
+        message: "{VALUE} is not a valid email",
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    confirmPassword: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function (v) {
+          return this.password === v;
+        },
+        message: "Passwords do not match",
+      },
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: function (v) {
+          return validator.isMobilePhone(v, "ar-EG");
+        },
+        message: "{VALUE} is not a valid phone number",
+      },
+    },
+
+    avatar: {
+      type: String,
+    },
+    Cart: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "cart",
+    },
+    address: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Address",
+    },
+    orders: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Order",
+      },
+    ],
+    Wishlist: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "product",
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("users", UserSchema);
