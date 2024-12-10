c63f505b9430fa7aca0d5a441b6fbc520a520bb4
noor hesham <101483584+noorhesham9@users.noreply.github.com>
2024-12-10 03:17:41 +0200

    Noor eldeen (#6)
    finish 90% of preparing to begin work of the backend

---
+++ server/utils/email.js
@@ -0,0 +1,26 @@
+// const nodemailer = require("nodemailer");
+
+// const sendEmail = async (option) => {
+//   // create a transporter
+//   var transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//       user: process.env.emailforEmail,
+//       pass: process.env.passwordforemail,
+//     },
+//   });
+
+//   var mailOptions = {
+//     from: process.env.emailforEmail,
+//     to: option.email,
+//     subject: option.subject,
+//     text: option.message,
+//   };
+
+//   console.log(option.message);
+//   console.log(option.subject);
+//   console.log(option.email);
+//   await transporter.sendMail(mailOptions);
+// };
+
+// module.exports = sendEmail;
