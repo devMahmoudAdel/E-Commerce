a1175ee90aaeaea901f536cee9c7c8eea721ba98
omarHassan <jadm7341@gmail.com>
2024-12-10 19:57:04 +0200

    adding models
    Owner Model
    Message Model
    Call contact Model
    Write contact Model

--- server/models/ConatctCallModel.js
+++ server/models/ConatctCallModel.js
@@ -1,1 +1,21 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose");  
+const Schema = mongoose.Schema;  
+const contactCallschema = new Schema({  
+    Heading: {  
+        type: String,  
+        required: true,  
+        trim: true  
+    },  
+    Descriptions: {  
+        type: String,  
+        required: true  
+    },  
+    Phone: 
+        {  
+            type: String,  
+            required: true,  
+            trim: true  
+        }  
+     
+});  
+module.exports = mongoose.model('ContactCall', contactCallschema);
