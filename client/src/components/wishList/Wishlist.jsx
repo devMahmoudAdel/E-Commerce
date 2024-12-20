4a8201c5274b4ee995c1ad811efd94e9b48a85a2
Mahmoud Adel <162546758+devMahmoudAdel@users.noreply.github.com>
2024-12-20 21:08:20 +0200

    Add to Cart component

--- client/src/components/wishList/Wishlist.jsx
+++ client/src/components/wishList/Wishlist.jsx
@@ -1,12 +1,13 @@
 import axios from "axios";
 import { useState ,useEffect} from "react";
-import { Box, Button, Container, Typography } from "@mui/material";
+import { Box, Button, CircularProgress, Container, Typography } from "@mui/material";
 import product1Image from "../../assets/productsimages/product1.jpg";
 import product2Image from "../../assets/productsimages/product2.jpg";
 import product3Image from "../../assets/productsimages/product3.jpg";
 import DeleteIcon from "@mui/icons-material/Delete";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import "./index.css";
+import { AddToCart } from "../AddToCart";
 function Wishlist() {
   const [wishlist, setWishlist] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -34,7 +35,7 @@ function Wishlist() {
     };
     const moveAllToCart = async () => {
       try {
-      await axios.post("https://your-api-endpoint.com/cart/move-all", {
+      await axios.post("/user/addToCart", {
       items: wishlist,
       });
       setWishlist([]);
@@ -42,115 +43,165 @@ function Wishlist() {
         console.error("Error moving items to cart:", error);
       }
     };
+    
+     if (loading) {
+       return (
+         <Box
+           sx={{
+             display: "flex",
+             justifyContent: "center",
+             alignItems: "center",
+             height: "100vh",
+           }}
+         >
+           <CircularProgress sx={{ color: "#be0d0d" }} />
+         </Box>
+       );
+     }
   return (
     <Container
       sx={{
         padding: "50px 6px",
       }}
     >
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
-        <Typography
-          sx={{
-            fontSize: "20px",
-            lineHeight: "26px",
-            textAlign: "center",
-            fontFamily: "Poppins, sans-serif",
-          }}
-        >
-          Wishlist {`(${wishlist.length})`}
-        </Typography>
+      {!(wishlist.length > 0) ? (
+        <>
+          <Typography
+            variant="h6"
+            sx={{ textAlign: "center", color: "#be0d0d" }}
+          >
+            Your wishlist is empty.
+          </Typography>
+          <Typography
+            variant="body1"
+            sx={{ textAlign: "center", margin: "10px auto 0" }}
+          >
+            Add items to your wishlist to start shopping.
+          </Typography>
+        </>
+      ) : (
+        <>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Typography
+              sx={{
+                fontSize: "20px",
+                lineHeight: "26px",
+                textAlign: "center",
+                fontFamily: "Poppins, sans-serif",
+              }}
+            >
+              Wishlist {`(${wishlist.length})`}
+            </Typography>
 
-        <button className="moveAllButton">Move All To Bag</button>
-      </Box>
+            <button
+              className="moveAllButton"
+              onClick={() => {
+                moveAllToCart();
+              }}
+            >
+              Move All To Bag
+            </button>
+          </Box>
 
-      <Box
-        sx={{
-          margin: "0 auto",
-          display: "grid",
-          gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))",
-          gridGap: "20px",
-          padding: "20px 0px",
-          "@media(max-width :340px)": {
-            gridGap: "30px",
-            gridTemplateColumns: "repeat(auto-fit, minmax(180px, 1fr))",
-          },
-          width: "100%",
-          justifyContent: "center",
-          flexWrap: "wrap",
-        }}
-      >
-        {wishlist.map((product) => (
           <Box
             sx={{
               margin: "0 auto",
-              maxWidth: "250px",
-              display: "flex",
-              justifyContent: "space-between",
-              flexDirection: "column",
+              display: "grid",
+              gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))",
+              gridGap: "20px",
+              padding: "20px 0px",
+              "@media(max-width :340px)": {
+                gridGap: "30px",
+                gridTemplateColumns: "repeat(auto-fit, minmax(180px, 1fr))",
+              },
+              width: "100%",
+              justifyContent: "center",
+              flexWrap: "wrap",
             }}
-            key={product.id}
           >
-            <Box
-              sx={{
-                position: "relative",
-                height: "270px",
-    
-                width: "100%",
-                overflow: "hidden",
-              }}
-            >
-              <button title="Delete" className="delete-btn">
-                <DeleteIcon />
-              </button>
-              <div
-                style={{
-                  width: "100%",
-                  overflow: "hidden",
-                  height: "calc(100% - 41px)",
-                  borderRadius: "5px",
-                  padding: "2px 2px",
-                  display: "flex",
-                  justifyContent: "center",
-                  background: "#dadada",
-                  alignItems: "center",
-                }}
-              >
-                <img
-                  style={{
-                    padding: "2px 2px",
-                    width: "100%",
-                  }}
-                  loading="lazy"
-                  src={product.images}
-                  alt={product.name}
-                />
-              </div>
-
-              <button title="addToCart" className="add-to-cart">
-                <ShoppingCartOutlinedIcon /> Add To Cart
-              </button>
-            </Box>
-
-            <div className="product-info">
-              <Typography
+            {wishlist.map((product) => (
+              <Box
                 sx={{
-                  fontFamily: "inherit",
+                  margin: "0 auto",
+                  maxWidth: "250px",
+                  display: "flex",
+                  justifyContent: "space-between",
+                  flexDirection: "column",
                 }}
-                className="product-name"
+                key={product.id}
+                onClick={() => window.location.href = `/ProductDetails/${product.id}`}
               >
-                {product.name}
-              </Typography>
-              <Typography className="price">${product.price}</Typography>
-            </div>
+                <Box
+                  sx={{
+                    position: "relative",
+                    height: "270px",
+
+                    width: "100%",
+                    overflow: "hidden",
+                  }}
+                >
+                  <button
+                    title="Delete"
+                    className="delete-btn"
+                    onClick={() => deleteWishlistItem(product.id)}
+                  >
+                    <DeleteIcon />
+                  </button>
+                  <div
+                    style={{
+                      width: "100%",
+                      overflow: "hidden",
+                      height: "calc(100% - 41px)",
+                      borderRadius: "5px",
+                      padding: "2px 2px",
+                      display: "flex",
+                      justifyContent: "center",
+                      background: "#dadada",
+                      alignItems: "center",
+                    }}
+                  >
+                    <img
+                      style={{
+                        padding: "2px 2px",
+                        width: "100%",
+                      }}
+                      loading="lazy"
+                      src={product.images}
+                      alt={product.name}
+                    />
+                  </div>
+
+                  <button
+                    title="addToCart"
+                    className="add-to-cart"
+                    onClick={() => AddToCart(product)}
+                  >
+                    <ShoppingCartOutlinedIcon /> Add To Cart
+                  </button>
+                </Box>
+
+                <div className="product-info">
+                  <Typography
+                    sx={{
+                      fontFamily: "inherit",
+                    }}
+                    className="product-name"
+                  >
+                    {product.name}
+                  </Typography>
+                  <Typography className="price">${product.price}</Typography>
+                </div>
+              </Box>
+            ))}
           </Box>
-        ))}
-      </Box>
+        </>
+      )}
     </Container>
   );
 }
